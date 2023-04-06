44729459be299f087741e069d6ab01ece8a07189
Alex Todeush <todeusha@tcd.ie>
2023-04-06 14:32:10 +0100

    Changed entry fields to spinbox

--- killer_calc.py
+++ killer_calc.py
@@ -6,7 +6,8 @@ of numbers for solving Killer Sudoku puzzles or similar games.
 import tkinter as tk
 import customtkinter as ctk
 import itertools
-from PIL import Image,ImageTk
+from PIL import Image
+from ctk_spinbox import FloatSpinbox
 
 buttons = []
 results = []
@@ -43,7 +44,6 @@ def calculate():
     # Display the results as clickable buttons
     if len(results) == 0:
         error_label.configure(text="There are no possible combinations.")
-        result_frame.configure(bg="white")
     else:
         for i, combination in enumerate(results):
             if len(combination) == 1:
@@ -119,7 +119,7 @@ ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 window = ctk.CTk()
 window.title("Killer Sudoku Combination Calculator")
-window.geometry("600x450")
+window.geometry("650x500")
 
 title_label = ctk.CTkLabel(window, text="Enter the cage sum and the number of cells in the cage:", font=ARIAL_14)
 title_label.pack(pady=10)
@@ -130,13 +130,13 @@ input_frame.pack(pady=10)
 sum_input_label = ctk.CTkLabel(input_frame, text="Cage Sum (1-45):", font=ARIAL_12, justify="center")
 sum_input_label.grid(row=0, column=0, padx=5, pady=5)
 
-sum_entry = ctk.CTkEntry(input_frame, font=ARIAL_14, justify="center")
+sum_entry = FloatSpinbox(input_frame, width = 150, height =50, start=1, end=45, step_size=1)
 sum_entry.grid(row=0, column=1, padx=5, pady=5)
 
 num_cell_label = ctk.CTkLabel(input_frame, text="Number of Cells (1-9):", font=ARIAL_12, justify="center")
 num_cell_label.grid(row=1, column=0, padx=5, pady=5)
 
-num_cells_entry = ctk.CTkEntry(input_frame, font=ARIAL_14, justify="center")
+num_cells_entry = FloatSpinbox(input_frame, width = 150, height =50, start=1, end=9, step_size=1)
 num_cells_entry.grid(row=1, column=1, padx=5, pady=5)
 
 calculate_button = ctk.CTkButton(window, text="Calculate Combinations", font=ARIAL_12, command=calculate)
@@ -153,7 +153,7 @@ grid_frame.pack(side="left")
 number_pad = NumberPad(dual_pane_frame)
 
 image = Image.open('assets/reset.jpg')
-image = image.resize((45,45), Image.ANTIALIAS)
+image = image.resize((45,45), Image.Resampling.LANCZOS)
 img = ctk.CTkImage(image, size=(45,45))
 
 reset_button = ctk.CTkButton(dual_pane_frame, text="", image=img, font=ARIAL_12, command=number_pad.reset_exclusions, height=50)
